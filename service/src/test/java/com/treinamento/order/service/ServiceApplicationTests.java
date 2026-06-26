5dbf76aace6ea5589149e0cf9afa6678dab3a438
Oliver Santos <zabsoluto234@gmail.com>
2026-06-26 19:51:56 -0300

    adiciona projeto base Spring Boot em service/

---
+++ service/src/test/java/com/treinamento/order/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.order.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
